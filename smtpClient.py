c128a71a6edcde7e12733f8d8e6dfb44f248df51
Rgr9409 <rgr9409@nyu.edu>
2026-02-16 13:21:51 -0500

    third attempt

--- smtpClient.py
+++ smtpClient.py
@@ -16,36 +16,51 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     recv = clientSocket.recv(1024).decode()
     #print(recv) #You can use these print statement to validate return codes from the server.
-    #if recv[:3] != '220':
-    #    print('220 reply not received from server.')
+    if recv[:3] != '220':
+        clientSocket.close()
+        return
 
     # Send HELO command and print server response.
     heloCommand = 'HELO Alice\r\n'
     clientSocket.send(heloCommand.encode())
     recv1 = clientSocket.recv(1024).decode()
-    print(recv1) 
-    #if recv1[:3] != '250':
-    #    print('250 reply not received from server.')
+    #print(recv1) 
+    if recv1[:3] != '250':
+        clientSocket.close()
+        return
 
     # Send MAIL FROM command and handle server response.
     # Fill in start
     mail_from = 'MAIL FROM:<alice@test.com>\r\n'
     clientSocket.send(mail_from.encode())
-    clientSocket.recv(1024)
+    recv2 = clientSocket.recv(1024).decode()
+    #print(recv2) 
+    if recv2[:3] != '250':
+        clientSocket.close()
+        return
     # Fill in end
 
     # Send RCPT TO command and handle server response.
     # Fill in start
     rcpt_to = 'RCPT TO:<bob@test.com>\r\n'
     clientSocket.send(rcpt_to.encode())
-    clientSocket.recv(1024)
+    recv3 = clientSocket.recv(1024).decode()
+    #print(recv3)
+    if recv3[:3] != '250':
+        clientSocket.close()
+        return
+    #clientSocket.recv(1024)
     # Fill in end
 
     # Send DATA command and handle server response.
     # Fill in start
     data_command = 'DATA\r\n'
     clientSocket.send(data_command.encode())
-    clientSocket.recv(1024)
+    recv4 =  clientSocket.recv(1024).decode()
+    #print(recv4)
+    if recv4[:3] != '354':
+        clientSocket.close()
+        return
     # Fill in end
 
     # Send message data.
@@ -56,14 +71,21 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Message ends with a single period, send message end and handle server response.
     # Fill in start
     clientSocket.send(endmsg.encode())
-    clientSocket.recv(1024)
+    recv5 = clientSocket.recv(1024).decode()
+    if recv5[:3] != '250':
+        clientSocket.close()
+        return
+    #clientSocket.recv(1024)
     # Fill in end
 
     # Send QUIT command and handle server response.
     # Fill in start
     quit_command = 'QUIT\r\n'
     clientSocket.send(quit_command.encode())
-    clientSocket.recv(1024)
+    recv6 = clientSocket.recv(1024).decode()
+    if recv6[:3] != '221':
+        clientSocket.close()
+        return
     clientSocket.close()
     # Fill in end
 
